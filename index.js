826fad233c2715d62bedf9d7998f86fe53749270
alfred kivuva <kivuvakioko97@gmail.com>
2024-01-09 21:15:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Callback function to be executed when the DOM is loaded
+  
+    // Target the paragraph element with id="text"
+    const textParagraph = document.getElementById('text');
+  
+    // Replace the text with "This is really cool!"
+    textParagraph.textContent = 'This is really cool!';
+  });
+
